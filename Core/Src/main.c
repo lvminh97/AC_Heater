b453b13ba2f6de1e674406eba7df966b41ebd71c
lvminh97 <lvminh97@gmail.com>
2022-07-23 15:29:34 +0700

    Change to rising edge for external interrupt event on INC_BTN and DEC_BTN pins
    Signed-off-by: lvminh97 <lvminh97@gmail.com>

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -274,7 +274,7 @@ static void MX_GPIO_Init(void)
 
   /*Configure GPIO pins : DEC_BTN_Pin INC_BTN_Pin */
   GPIO_InitStruct.Pin = DEC_BTN_Pin|INC_BTN_Pin;
-  GPIO_InitStruct.Mode = GPIO_MODE_IT_FALLING;
+  GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
