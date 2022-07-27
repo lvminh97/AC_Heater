6f724187946db5cc15973ff6df3b80900ad858b7
lvminh97 <lvminh97@gmail.com>
2022-07-28 04:24:53 +0700

    Remove unused code
    Signed-off-by: lvminh97 <lvminh97@gmail.com>

--- Core/Src/max6675.c
+++ Core/Src/max6675.c
@@ -5,19 +5,8 @@ extern uint16_t raw_temp;
 
 float max6675_get_temp(void){
 	uint16_t res = 0, id;
-	uint8_t buffer[2] = {0};
-	
-//	HAL_GPIO_WritePin(SPI1_CS_GPIO_Port, SPI1_CS_Pin, GPIO_PIN_RESET);	// Set CS pin to low to select slave
-//	HAL_SPI_Receive(hspi, buffer, 2, 1000);
-//	HAL_GPIO_WritePin(SPI1_CS_GPIO_Port, SPI1_CS_Pin, GPIO_PIN_SET);		// Set CS pin to high to deselect slave
-	
-//	res = buffer[1];
-//	res = (res << 8) | buffer[0];
-//	raw_temp = res;
-//	res >>= 3;
 	
 	HAL_GPIO_WritePin(SPI1_CS_GPIO_Port, SPI1_CS_Pin, GPIO_PIN_RESET);	// Set CS pin to low to select slave
-	
 	for(id = 0; id < 16; id++){
 		HAL_GPIO_WritePin(SPI1_SCK_GPIO_Port, SPI1_SCK_Pin, GPIO_PIN_RESET);
 		HAL_Delay(1);
@@ -28,10 +17,6 @@ float max6675_get_temp(void){
 		HAL_GPIO_WritePin(SPI1_SCK_GPIO_Port, SPI1_SCK_Pin, GPIO_PIN_SET);
 		HAL_Delay(1);
 	}
-	
 	HAL_GPIO_WritePin(SPI1_CS_GPIO_Port, SPI1_CS_Pin, GPIO_PIN_SET);		// Set CS pin to high to deselect slave
-	
-	res >>= 3;
-	
-	return res * 0.25;
+	return (res >> 3) * 0.25;
 }
